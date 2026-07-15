b128d5b6a35fea0096f3bf0ccb38664ba68af2b0
GithinjiRichard <githinji.richard@outlook.com>
2026-07-15 21:42:44 +0300

    fix: HistoricalEvent import

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -17,6 +17,7 @@ import type {
   PerspectiveItem,
   PerspectiveKey,
   YearPoint,
+  HistoricalEvent,
 } from "./types";
 
 // ---------------------------------------------------------------------------
